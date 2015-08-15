cc100020efd4d27ce48e99792eac57f6214913ed
tarekkazak <tarek.kazak@gmail.com>
2015-08-15 22:56:40 +0000

    re-enabled mauela entry of letters

--- public/js/components/imageLoader.jsx
+++ public/js/components/imageLoader.jsx
@@ -5,30 +5,34 @@ var imageService;
 var images = (<div/>);
 var searchTerm = '';
 
+function fetchImages() {
+    imageService = imageServiceProvider.get(this.props.local);
+    var self = this; 
+
+    
+    function searchComplete(results) {
+        console.log(results);
+        if(results && results.length) {
+            images = _.map(results, (item) =>{
+                return (
+                        <img key={item.url}width="300" height="300" src={item.url} />
+                       );
+            });
+
+            self.forceUpdate();
+        }
+    }
+        
+    if(searchTerm !== this.props.word){
+        searchTerm = this.props.word; 
+        imageService.search(searchTerm).then(searchComplete);
+    }
+
+}
+
 var ImageLoader = React.createClass({
-    componentDidUpdate : function() {
-        imageService = imageServiceProvider.get(this.props.local);
-        var self = this; 
-
-       
-        function searchComplete(results) {
-            console.log(results);
-            if(results && results.length) {
-                images = _.map(results, (item) =>{
-                    return (
-                            <img key={item.url}width="300" height="300" src={item.url} />
-                           );
-                });
-
-                self.forceUpdate();
-            }
-        }
-            
-        if(searchTerm !== this.props.word){
-            searchTerm = this.props.word; 
-            imageService.search(searchTerm).then(searchComplete);
-        }
-    },
+    componentDidMount : fetchImages,
+    componentDidUpdate : fetchImages,
     render : function() {
 
         return (
