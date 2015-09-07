3cf59c58523d5bbb2add196d8b2d121485274699
tarekkazak <tarek.kazak@gmail.com>
2015-09-07 04:20:20 +0000

    testing  azure

--- app/index.js
+++ app/index.js
@@ -24,6 +24,6 @@ for (let route of letterRoutes) {
 }
 
 
-app.listen(process.env.PORT || 8080, function() {
+app.listen(process.env.PORT || 5500, function() {
     console.log('running on ', process.env.PORT);   
 });
