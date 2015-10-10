3ebb67b50ae9d773df0120fb474322bf2053e65d
tarekkazak <tarek.kazak@gmail.com>
2015-10-10 22:25:55 +0000

    started using FP in dataModel

--- public/js/model/dataModel.js
+++ public/js/model/dataModel.js
@@ -1,3 +1,8 @@
+var monad = require('folktale/control').monads;
+var Maybe = require('folktale/data').Maybe;
+var _ = require('lodash');
+
+var joinToLower = _.compose((x) => x.toLowerCase(), (x) => x.join(''));
 export class DataModel {
 
     constructor() {
@@ -6,35 +11,43 @@ export class DataModel {
         this.allowedWords = [];
         this.letters = [];
         this.localImageUrl = '/images/';
+        this.init();
     }
 
     init() {
         for(let d in this.data.words) {
-            dataMap.set(d, data.words[d]);
+            this.dataMap.set(d, this.data.words[d]);
             this.allowedWords.push(d);
         }
     }
 
-
     containsWord(word) {
-        word = word.toLowerCase();
-        return dataMap.has(word);
+        return Maybe.of(word).map((x) => x.toLowerCase()).map(this.hasWordData.bind(this)).join();
     };
 
     resetWord() {
         this.letters = [];
     };
+    
+    hasWordData(word) {
+        return this.dataMap.has(word);
+    };
+
+    getWordData(word) {
+        return Maybe.fromNullable(this.dataMap.get(word));
+    };
+
+    hasLocation(item) {
+        return Maybe.fromNullable(item.location);
+    }
 
     isResourceLocal() {
-        var word = this.letters.join('').toLowerCase();
-        var wordData = dataMap.get(word);
-        return wordData ? wordData.location === 'local' : false;
+        return  Maybe.of(this.letters).map(joinToLower).
+            chain(this.getWordData.bind(this)).map(hasLocation);
     };
 
     getSearchTerm(word) {
-        word = word.toLowerCase();
-        var wordData = dataMap.get(word);
-        return wordData.searchTerm ? wordData.searchTerm : word;
+        return Maybe.of(word).map((x) => x.toLowerCase()).map(this.getWordData.bind(this)).map((x) => x.searchTerm || word).join();
     };
 
 
