2ea8faf1f08f004fde263637ebe7e6270adbba2a
tarekkazak <tarek.kazak@gmail.com>
2015-10-11 21:16:01 +0000

    model refactor

--- public/js/model/dataModel.js
+++ public/js/model/dataModel.js
@@ -2,7 +2,6 @@ var monad = require('folktale/control').monads;
 var Maybe = require('folktale/data').Maybe;
 var _ = require('lodash');
 
-var joinToLower = _.compose((x) => x.toLowerCase(), (x) => x.join(''));
 export class DataModel {
 
     constructor() {
@@ -12,6 +11,7 @@ export class DataModel {
         this.letters = [];
         this.localImageUrl = '/images/';
         this.init();
+        this.wordDataCompose = _.compose(monad.chain(this.getWordData.bind(this)), monad.map((x) => x.toLowerCase()));
     }
 
     init() {
@@ -22,52 +22,41 @@ export class DataModel {
     }
 
     containsWord(word) {
-        return Maybe.of(word).map((x) => x.toLowerCase()).map(this.hasWordData.bind(this)).join();
+        return !this.wordDataCompose(Maybe.of(word)).isNothing;
     };
 
     resetWord() {
         this.letters = [];
     };
     
-    hasWordData(word) {
-        return this.dataMap.has(word);
-    };
-
     getWordData(word) {
         return Maybe.fromNullable(this.dataMap.get(word));
     };
 
-    hasLocation(item) {
-        return Maybe.fromNullable(item.location);
-    }
 
     isResourceLocal() {
-        return  Maybe.of(this.letters).map(joinToLower).
-            chain(this.getWordData.bind(this)).map(hasLocation);
+        function getLocation(item) {
+            return Maybe.fromNullable(item.location);
+        }
+
+        return this.wordDataCompose(Maybe.of(this.letters.join('')))
+                .chain((x) => x === 'local');
     };
 
     getSearchTerm(word) {
-        return Maybe.of(word).map((x) => x.toLowerCase()).map(this.getWordData.bind(this)).map((x) => x.searchTerm || word).join();
+        return this.wordDataCompose(Maybe.of(word))
+                .chain((x) => x.searchTerm || word);
     };
 
 
     wordComplete(){
         var word = this.letters.join('').toLowerCase();
 
-        function wordIsValid(word) {
-           var valid = false;
-           for(let w of this.allowedWords) {
-               if(w.length >= word.length) {
-                   valid = true;
-               }
-           }
-           return valid;
-        }    
         
         if(this.letters.length < 1) {
             return false;
         }
-        return this.containsWord(word) || !wordIsValid(word);
+        return this.containsWord(word);
     };
 
 }
